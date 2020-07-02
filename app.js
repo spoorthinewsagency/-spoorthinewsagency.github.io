5e5571a21f4864342cfbd154c6442adbd1dbbee3
K B Hemanth Raj <hemanthrajkb005@gmail.com>
2020-07-02 17:11:47 +0530

    v4

--- app.js
+++
@@ -1,8 +0,0 @@
-//check the Phone Number
-var rootRef = firebase.database().ref();
-rootRef.once("value")
-.then(function(snapshot) {
-var key1 = snapshot.child("Users").child("Customers").child(uid).child("UserInformation").child("phoneNumber").key;
-var val1 = snapshot.child("Users").child("Customers").child(uid).child("UserInformation").child("phoneNumber").val();
-console.log(key1);
-console.log(val1);
